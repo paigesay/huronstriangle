0ee24c96f9cb966ad2162f059349c217b2ce7a6d
Paige Saychareun <143040055+paigesay@users.noreply.github.com>
2025-03-09 00:01:48 -0800

    Created and imported code for r.sh

---
+++ r.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Program name: "Huron’s Triangles". A short description of the purpose of the program
+# Copyright (C) 2025  Paige Saychareun
+
+# This file is part of the software program "Program Name".
+
+# "Program Name" is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# "Program Name" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+
+# Author information
+#   Author name : Paige Saychareun
+#   Author email: psaychareun@csu.fullerton.edu
+#   Author section: 240-9
+#   Author CWID : 
+
+echo "Cleaning up previous build files..."
+rm -f triangle
+
+echo "Compiling C/C++ main file..."
+gcc -c -m64 -no-pie -o triangle.o triangle.c
+
+echo "Assembling manager.asm..."
+nasm -f elf64 -o manager.o manager.asm
+
+echo "Assembling istriangle.asm..."
+nasm -f elf64 -o istriangle.o istriangle.asm
+
+echo "Assembling huron.asm..."
+nasm -f elf64 -o huron.o huron.asm
+
+echo "Linking all object files..."
+gcc -m64 -no-pie -o triangle triangle.o manager.o istriangle.o huron.o -lm
+
+echo "Build successful! Running the program..."
+./triangle
