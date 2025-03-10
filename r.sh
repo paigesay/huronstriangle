3a1cfe8454cef146d26b4258c6959a86483e9076
Paige Saychareun <143040055+paigesay@users.noreply.github.com>
2025-03-09 23:06:04 -0700

    Updated header

--- r.sh
+++ r.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Program name: "Huron’s Triangles". A short description of the purpose of the program
+# Program name: "Huron’s Triangles". Bash file.
 # Copyright (C) 2025  Paige Saychareun
 
-# This file is part of the software program "Program Name".
+# This file is part of the software program "Huron's Triangle".
 
-# "Program Name" is free software: you can redistribute it and/or modify
+# "Huron's Triangle" is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# "Program Name" is distributed in the hope that it will be useful,
+# "Huron's Triangle" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
@@ -22,7 +22,7 @@
 #   Author name : Paige Saychareun
 #   Author email: psaychareun@csu.fullerton.edu
 #   Author section: 240-9
-#   Author CWID : 
+#   Author CWID : 884902354
 
 echo "Cleaning up previous build files..."
 rm -f triangle
